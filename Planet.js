947bafc916406a406573dd1ff5f38bb5d03dd6ea
YL2407 <younes_laribi@bisad.ae>
2022-04-16 17:02:35 +0400

    changed to both planets moving

--- Planet.js
+++ Planet.js
@@ -10,11 +10,28 @@ class Planet{
         this.sy = sy;
         this.g = g;
     }
-    update(){
+    update(starMass,sx,sy){
+        this.starMass = starMass;
+        this.sx = sx;
+        this.sy = sy;
         this.r = Math.sqrt(Math.pow((this.xpos-this.sx),2)+Math.pow((this.ypos-this.sy),2))
         this.normvecx = (this.sx-this.xpos)/this.r;
         this.normvecy = (this.sy-this.ypos)/this.r;
         this.force = (this.g*this.starMass/this.r**2);
+        this.acc = [this.normvecx*this.force,this.normvecy*this.force]
+        this.vel[0]+=this.acc[0];
+        this.vel[1]+=this.acc[1];
+        this.xpos+=this.vel[0];
+        this.ypos+=this.vel[1];
+        stroke(255);
+        strokeWeight(3);
+        //equation on teams
+    }
+    addForce(){
+        this.r = Math.sqrt(Math.pow((this.xpos-this.sx),2)+Math.pow((this.ypos-this.sy),2))
+        this.normvecx = (this.sx-this.xpos)/this.r;
+        this.normvecy = (this.sy-this.ypos)/this.r;
+        this.force += (this.g*this.starMass/this.r**2);
         this.acc = [this.normvecx*this.force,this.normvecy*this.force]
         this.vel[0]+=this.acc[0];
         this.vel[1]+=this.acc[1];
